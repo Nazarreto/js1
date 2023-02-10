42dd25eafb081f42c9c03d892c0a9e0cae83fee0
Nazarreto <102985652+Nazarreto@users.noreply.github.com>
2023-02-10 20:59:00 +0200

    create js

---
+++ script.js
@@ -0,0 +1,25 @@
+let city
+city = 'Київ'
+city = 'Львів'
+
+let address = city
+
+alert(address)
+
+let n = 3
+
+let result = n*3
+
+console.log(result)
+
+let pen = 5.25
+
+let pencil = 3.50
+
+let numberPen = 4
+
+let numberPencil = 6
+
+let result2 = pen * numberPen + (pencil * numberPencil)
+
+document.write(result2)
